d253f4b634bdae936398a44bd639319397980020
Fredrik Strömbäck <fredde.stromback@gmail.com>
2021-03-19 01:26:40 +0100

    adds error handling and env-variables

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const express = require('express')
 const app = express()
 
+require('dotenv').config({ path: '.env' })
+
 const port = 4000
 
 app.get('/', require('./routes/indexRouter'))
+
+app.use((req, res, next) => {
+  res.status(404).json('Not Found')
+})
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500)
+  res.send(err.message || 'Internal Server Error')
+})
 
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`)
