a7505bce4eef07cc8f3eeb8005bea30ad8fae52a
Fredrik Strömbäck <fredde.stromback@gmail.com>
2021-03-19 01:05:20 +0100

    simple hello world api

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log('hello world!')
+const express = require('express')
+const app = express()
+
+const port = 4000
+
+app.get('/', (req,res) => {
+  res.send('hello world')
+})
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`)
+})
