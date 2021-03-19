98938586fe510c4be3e72c7bc0a7e36f3e73b357
Fredrik Strömbäck <fredde.stromback@gmail.com>
2021-03-19 01:16:30 +0100

    adds router and controller for index-route

--- app.js
+++ app.js
@@ -3,9 +3,7 @@ const app = express()
 
 const port = 4000
 
-app.get('/', (req,res) => {
-  res.send('hello world')
-})
+app.get('/', require('./routes/indexRouter'))
 
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`)
---
+++ controllers/indexController.js
@@ -0,0 +1,7 @@
+const indexController = {}
+
+indexController.index = (req,res,next) => {
+  res.json('Index Success')
+}
+
+module.exports = indexController
