db77e75619433cabab05167807d4cabad3db662f
Fredrik Strömbäck <fredde.stromback@gmail.com>
2021-03-21 22:38:17 +0100

    adds a simple header

--- client/hue-client/src/App.vue
+++ client/hue-client/src/App.vue
@@ -1,10 +1,17 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <Header />
 </template>
+
+<script>
+import Header from './components/Header'
+
+export default {
+  name: 'App',
+  components: {
+    Header
+  }
+}
+</script>
 
 <style>
 #app {
---
+++ client/hue-client/src/components/Header.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>Hue-Vue</h1>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
